a3b0ceb1d1203664a14cd90f13f8521803a0ad5d
jugne <ugne.stolz@bsse.ethz.ch>
2025-06-26 20:46:34 +0200

    not done yet

--- src/starbeast3/evolution/branchratemodel/SharedSpeciesClockModel.java
+++ src/starbeast3/evolution/branchratemodel/SharedSpeciesClockModel.java
@@ -1,13 +1,14 @@
 package starbeast3.evolution.branchratemodel;
 
+import beast.base.evolution.branchratemodel.BranchRateModel;
 import beast.base.inference.CalculationNode;
 import beast.base.core.Input;
 
 public class SharedSpeciesClockModel extends CalculationNode {
 	
 	final public Input<BranchRateModelSB3> branchRateModelInput = new Input<>("branchRateModel", "Species tree branch rate model", Input.Validate.REQUIRED);
-	
 
+	final public Input<BranchRateModelSB3> morphBranchRateModelInput = new Input<>("morphBranchRateModel", "Species tree branch rate model for morphological data", Input.Validate.OPTIONAL);
 	
 
 	@Override
